631928de54ba2416891f2f2a74ccb1be07965f56
Steven Haddox <steven@haddox.us>
2009-10-28 00:36:55 -0400

    attempted to add less css - this may need to be commented out and installed manually if I cannot get it resolved.

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'jnunemaker-twitter', :lib => 'twitter', :source => 'http://gems.github.com'
   config.gem 'formtastic', :source => 'http://gemcutter.org'
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'less'
+  config.gem 'less', :version => '~> 1.2.11', :source => 'http://gemcutter.org'
   config.gem 'uuid'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
