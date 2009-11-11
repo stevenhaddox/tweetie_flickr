0039239d82572f425424c63ca71c1fcf0010f1f5
Steven <steven@slk-macpro.home>
2009-11-11 02:02:05 -0500

    lots of UI tweaks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-
+  
   PAGES = ['index','join']
 
   def index
+    f4t_search = Twitter::Search.new.from('flickr4tw1tter')
+    @recent_tweets = f4t_search.map { |result| {:tweet=>result.text,:date=>result.created_at} }
+    
     # render the landing page
   end
 
