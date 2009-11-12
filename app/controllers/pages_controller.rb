cb253652fe95540967bf8b3ed3ff912784789763
Steven Haddox <steven@slkdemos.com>
2009-11-12 21:14:03 +0000

    added some safety checks to the index view in case we get a JSON parse error on tweets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
   def index
     f4t_search = Twitter::Search.new.from('flickr4tw1tter')
-    @recent_tweets = f4t_search.map { |result| {:tweet=>result.text,:date=>result.created_at} }
+    @recent_tweets = f4t_search.map { |result| {:tweet=>result.text,:date=>result.created_at} } unless f4t_search.blank?
     
     # render the landing page
   end
