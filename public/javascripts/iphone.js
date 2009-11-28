298a01ef6a40eab4279d463af42382fcbacc0416
Steven Haddox <steven@dell-netbook.(none)>
2009-11-28 01:52:39 -0500

    more iPhone UI testing

---
+++ public/javascripts/iphone.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $.jQTouch({
+    icon: 'jqtouch.png',
+    statusBar: 'black-translucent',
+    preloadImages: [
+      'img/chevron.png',
+      'img/backButton.png'
+    ]
+  });
+
+});
