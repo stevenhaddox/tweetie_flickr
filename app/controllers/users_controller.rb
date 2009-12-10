38e18e4fe4629c05f612facd23a1f1eefab91802
Steven Haddox <steven@slkdemos.com>
2009-12-10 11:22:04 +0000

    fixed an exception error when username wasn't found in Flickr, 404 returned.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,11 +32,13 @@ class UsersController < ApplicationController
         if flickr_username.match('/')
           flickr_username = flickr_username.gsub('http://flickr.com/people/','').gsub('/','')
         end
-        new_flickr_id = check_flickr_user_id(flickr_username)
-        if new_flickr_id.blank?
-          params[:user].delete(:flickr_username)
-        else
-          params[:user][:flickr_user_id] = new_flickr_id
+        unless flickr_username == @user.flickr_username
+          new_flickr_id = check_flickr_user_id(flickr_username)
+          if new_flickr_id.blank?
+            params[:user].delete(:flickr_username)
+          else
+            params[:user][:flickr_user_id] = new_flickr_id
+          end
         end
       end
       # either reset / update custom_client_hash - can't do both
@@ -48,10 +50,13 @@ class UsersController < ApplicationController
 
       @user.update_attributes(params[:user])
     end
+
+    render :action => :edit and return false unless flash[:error].blank?
     unless @user.flickr_token
       redirect_to @flickr.auth.url(:write) and return
-    else      
+    else
       respond_to do |format|
+        flash[:success]="Profile Updated Successfully"
         format.html { redirect_to user_path(@user) }
       end
     end
@@ -106,13 +111,16 @@ private
 
   def check_flickr_user_id(flickr_username)
     # no need to update the flickr_user_id if it is the same
-    if @user.flickr_username == flickr_username
+    if current_user.flickr_username == flickr_username
       return nil
     end
-    flickr_id = User.get_flickr_id(flickr_username)
-    unless flickr_id
-      flash[:error] = "There was an error looking up your ID"
-      render :action => :edit and return nil
+    begin 
+      flickr_id = User.get_flickr_id(flickr_username)
+    rescue 
+      flash[:error] = "There was an error looking up your Flickr ID<br />
+        Please verify you entered only your username seen in your Flickr profile URL:<br />
+        http://flickr.com/people/<strong>username</strong>/"
+      return
     end
     flickr_id
   end
