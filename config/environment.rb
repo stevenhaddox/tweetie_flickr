6b1b0edf22fc80552004eebce58cfceeda0e852d
Steven Haddox <steven@haddox.us>
2010-06-13 02:51:51 -0400

    more heroku debugging

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  skip_before_filter :generate_css_from_less if Rails.env.production?
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
